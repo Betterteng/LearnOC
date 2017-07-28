c39cc1630ba92ad2826840941646f5af9ed17184
Betterteng <betterteng@gmail.com>
2017-07-28 16:48:36 +1000

    First step

---
+++ Test1/Test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test1
+//
+//  Created by 滕施男 on 28/7/17.
+//  Copyright © 2017 TENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test1/Test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test1
+//
+//  Created by 滕施男 on 28/7/17.
+//  Copyright © 2017 TENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
